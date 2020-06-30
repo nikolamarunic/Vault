a6362238c4f2e305ca48a28d51f6b28610457299
Nikola Marunic <nikimarunic@gmail.com>
2020-06-30 10:43:15 -0400

    Created basic model and did some test CRUD with django admin interface

--- backend/vault/admin.py
+++ backend/vault/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vault # add this
+
+class VaultAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
 # Register your models here.
+admin.site.register(Vault, VaultAdmin)
--- backend/vault/models.py
+++ backend/vault/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Vault(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+
+    def _str_(self):
+        return self.name
