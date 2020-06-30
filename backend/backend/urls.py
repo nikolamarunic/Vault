34b4ab8176435b7bd61d81e5a2f18460d7c7479b
Nikola Marunic <nikimarunic@gmail.com>
2020-06-30 14:30:44 -0400

    finished basic backend. TODO: Authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include                
+from rest_framework import routers                   
+from vault import views                           
+
+router = routers.DefaultRouter()                      
+router.register(r'items', views.VaultView, 'item')     
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),         path('api/', include(router.urls))             
 ]
--- backend/vault/views.py
+++ backend/vault/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets          
+from .serializers import VaultSerializer      
+from .models import Item
+
+class VaultView(viewsets.ModelViewSet):       
+    serializer_class = VaultSerializer          
+    queryset = Item.objects.all()              
