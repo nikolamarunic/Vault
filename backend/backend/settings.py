f7076cca2be8a0bda60f8cfdeb9591b37c523d59
Nikola Marunic <nikimarunic@gmail.com>
2020-06-30 18:28:27 -0400

    removed instances of old django key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from .secret_settings import SECRET_KEY
-# SECRET_KEY = '6dhmm2h_x9h)9ng*hm#e&842xf_$6sdzcsb8=-i8zk+iu22z&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
