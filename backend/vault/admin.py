730763134bdd0a440ba58988a9cb55f1fc1a03a1
Nikola Marunic <nikimarunic@gmail.com>
2020-06-30 10:45:16 -0400

    changed vaults to items

--- backend/vault/admin.py
+++ backend/vault/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Vault # add this
+from .models import Item # add this
 
-class VaultAdmin(admin.ModelAdmin):
+class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 
 # Register your models here.
-admin.site.register(Vault, VaultAdmin)
+admin.site.register(Item, ItemAdmin)
