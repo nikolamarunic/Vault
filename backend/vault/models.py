214b63aa9587c4ad99c6a6c0cc7c22a1e45ba50a
Nikola Marunic <nikimarunic@gmail.com>
2020-07-02 10:30:09 -0400

    remade models.py to have each item include which user created it

--- backend/vault/models.py
+++ backend/vault/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #Deletes item if user is deleted
 
     def _str_(self):
         return self.name
