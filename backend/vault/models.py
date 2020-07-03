c89c641609f1b27f746167ec061b8b61c2d0a570
Nikola Marunic <nikimarunic@gmail.com>
2020-07-03 11:02:12 -0400

    changed the model and view

--- backend/vault/models.py
+++ backend/vault/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User  
 
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
+    owner = models.ForeignKey(
+        User, related_name="todos", on_delete=models.CASCADE, null=True)  
 
     def _str_(self):
         return self.name
--- backend/vault/views.py
+++ backend/vault/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets          
+from rest_framework import viewsets, permissions          
 from .serializers import VaultSerializer      
 from .models import Item
 
 class VaultView(viewsets.ModelViewSet):       
     serializer_class = VaultSerializer          
-    queryset = Item.objects.all()              
+    # queryset = Item.objects.all()      #Used before when we wanted to display ALL the items
+    permission_classes = [permissions.IsAuthenticated]  # For authenticating a user        
+
+    def get_queryset(self): 
+        return self.request.user.items.all()
+
+    def perform_create(self, serializer):  
+        serializer.save(owner=self.request.user)
